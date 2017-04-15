94a8291bb032d2481bd04b3036e7c5ae429e6f78
Nick Hajek <nhajek16@gmail.com>
2017-04-15 02:10:55 -0400

    These are the .java files

---
+++ Project/app/src/main/java/ant/catch_up/Activity_Login.java
@@ -0,0 +1,34 @@
+package ant.catch_up;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Activity_Login extends AppCompatActivity {
+
+    public void callRegisterPage(View view) {
+        Intent i=new Intent(Activity_Login.this, Activity_Register.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
