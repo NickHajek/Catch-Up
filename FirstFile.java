0ae173d575ad7bed0f9fe7cdf401b24ce397056b
Nick Hajek <nhajek16@gmail.com>
2017-04-14 22:15:54 -0400

    Here's our first file

---
+++ FirstFile.java
@@ -0,0 +1,11 @@
+/*
+
+Hey guys! This is our first java script (not JavaScript) file.
+
+...I just want something to show up properly on GitHub :(
+
+Author check-in! Please add your names so I can see that you can see this file!
+
+Authors:
+	Nick Hajek 
+*/
